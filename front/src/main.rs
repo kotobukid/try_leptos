f473e95bc6215e55c39474bfc50b17aff8dc12d5
kotobukid <kotobuki@daqua.jp>
2025-05-21 01:40:36 +0900

    カウンターを増やせる仕組み

--- front/src/main.rs
+++ front/src/main.rs
@@ -8,12 +8,30 @@ use leptos::prelude::*;
 fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
+
+    // カウンターのリストを管理するSignalVecを作成
+    let (counters, set_counters) = signal(vec![0, 100]);
     let (label_text, _) = signal(String::from("Custom Button"));
 
     mount_to_body(move || {
         view! {
             <div>
-                <SimpleCounter initial_value=0 step=10/>
+                <button
+                    on:click=move |_| {
+                        let mut current = counters.get();
+                        current.push(current.len() as i32 * 100);
+                        set_counters.set(current);
+                    }
+                >"Add Counter"</button>
+                <br />
+                <For each=move || counters.get()
+                    key=|count| *count
+                    children=move |initial_value| {
+                        view! {
+                            <SimpleCounter initial_value=initial_value step=10/>
+                        }
+                    }
+                />
                 <br />
                 <ButtonCustom num={1000} label=label_text />
             </div>
